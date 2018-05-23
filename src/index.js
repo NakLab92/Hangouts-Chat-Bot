3e229a8bdfd9d94cabcf4580e4e84b941909e891
Lypka Bohdan <kolaros92@gmail.com>
2018-05-23 23:56:36 +0300

    Added a new command for the bot

--- src/index.js
+++ src/index.js
@@ -1,15 +1,14 @@
 const TelegramBot = require('node-telegram-bot-api');
-const token = '519970076:AAEDzWGPuzBKWWYnv_B2f4h2BFklaqiES8o';
+const token = '566558605:AAFmS8-lMljZm_wyLhEttpw-QspD-mJles0';
 
 const bot = new TelegramBot(token, {polling: true});
 
-bot.onText(/\/start/, (msg) => {
-    // 'msg' is the received Message from Telegram
-    // 'match' is the result of executing the regexp above on the text content
-    // of the message
-
+bot.onText(/\/new/, (msg) => {
     const chatId = msg.from.id;
+bot.sendMessage(chatId, 'https://hangouts.google.com/call/');
+});
 
-// send back the matched "whatever" to the chat
+bot.onText(/\/old/, (msg) => {
+    const chatId = msg.from.id;
 bot.sendMessage(chatId, 'https://hangouts.google.com/call/npvpswx7bvdgdbewycgf6lmnfqe?no_rd');
 });
