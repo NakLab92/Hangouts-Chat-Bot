a63e5beb3a622fc96a8e7bc2143b48ca2ec20257
Lypka Bohdan <kolaros92@gmail.com>
2018-05-23 23:55:34 +0300

    Created a command for the chat

--- src/index.js
+++ src/index.js
@@ -3,7 +3,13 @@ const token = '519970076:AAEDzWGPuzBKWWYnv_B2f4h2BFklaqiES8o';
 
 const bot = new TelegramBot(token, {polling: true});
 
-bot.on('message', function (msg) {
-    const id = msg.from.id;
-    bot.sendMessage(id, 'https://hangouts.google.com/call/npvpswx7bvdgdbewycgf6lmnfqe?no_rd');
+bot.onText(/\/start/, (msg) => {
+    // 'msg' is the received Message from Telegram
+    // 'match' is the result of executing the regexp above on the text content
+    // of the message
+
+    const chatId = msg.from.id;
+
+// send back the matched "whatever" to the chat
+bot.sendMessage(chatId, 'https://hangouts.google.com/call/npvpswx7bvdgdbewycgf6lmnfqe?no_rd');
 });
